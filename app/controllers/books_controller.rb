75b3f6c6af9d0d15072f41b6d99288e679dd4440
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-03 14:35:40 -0400

    Render books in json

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,9 @@ class BooksController < ApplicationController
         csv_data = Book.generate_csv(@books)
         render plain: csv_data
       end
-
+      format.json do
+        render json:@books
+      end
     end
   end
 
